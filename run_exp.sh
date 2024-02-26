9a8c3f5a1cfb013369c43f94930142d66ac6832b
Jechin <jechinyu@163.com>
2024-02-27 01:02:23 +0800

    Update bash file

--- run_exp.sh
+++ run_exp.sh
@@ -14,13 +14,13 @@ python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 1 -N 2
 # python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 3 -N 20
 
 python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 1 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 2 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 3 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 2 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --adp_noise --seed 3 -N 6 -data prostate
 
 python fed_main.py --adaclip --epsilon 10 --mode no_dp --adp_round --adp_noise --seed 1 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode no_dp --adp_round --adp_noise --seed 2 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode no_dp --adp_round --adp_noise --seed 3 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode no_dp --adp_round --adp_noise --seed 2 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode no_dp --adp_round --adp_noise --seed 3 -N 6 -data prostate
 
 python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 1 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 2 -N 6 -data prostate
-python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 3 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 2 -N 6 -data prostate
+# python fed_main.py --adaclip --epsilon 10 --mode dpsgd --adp_round --seed 3 -N 6 -data prostate
